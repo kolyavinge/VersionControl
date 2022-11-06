fbce70b5cc42fe8bceb6294b089ff06107289fe0
Vinge Nikolay <vn2756@gmail.com>
2022-11-06 22:00:17 +0400

    [assembly: InternalsVisibleTo("Luna.Tests.IDE.App")]

--- VersionControl/Assembly.cs
+++ VersionControl/Assembly.cs
@@ -4,4 +4,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("VersionControl.Tests")]
 [assembly: InternalsVisibleTo("Luna.Tests.IDE")]
+[assembly: InternalsVisibleTo("Luna.Tests.IDE.App")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
