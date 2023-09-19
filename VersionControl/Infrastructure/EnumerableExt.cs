39a2ae46913ccd7a28e76b590052a4a8e7ab52b4
Vinge Nikolay <vn2756@gmail.com>
2023-09-19 21:33:43 +0400

    fix

---
+++ VersionControl/Infrastructure/EnumerableExt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace VersionControl.Infrastructure;
+
+internal static class EnumerableExt
+{
+    public static void Each<T>(this IEnumerable<T> collection, Action<T> action)
+    {
+        foreach (var item in collection)
+        {
+            action(item);
+        }
+    }
+}
